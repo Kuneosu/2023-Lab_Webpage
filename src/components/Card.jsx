a84501dd11da7e04c00399f885e7bc4b8f6ba20c
Kuneosu <kks92155784@gmail.com>
2023-12-21 14:03:31 +0900

    CSS 세부 조정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = () => {
                 <span id="card__mac">Mac : dfb172feff65112c</span>
             </div>
             <div id="card__body">
-
+                <iframe src="http://localhost:3000/d-solo/ae85ca87-5c5d-4ff5-9038-90d32b387b1f/lab-data?orgId=1&refresh=1s&panelId=10" width="450" height="200" frameborder="0"></iframe>
             </div>
         </div>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,25 +5,27 @@ import "./styles/Login.css"
 const Login = () => {
     const navigate = useNavigate();
     return (
-        <div id="login__container">
-            <div className="login__wrapper" id="wrapper__left">
-                <div id="login__header">
-                    <span id="company__name">T/O Solution</span>
-                    <span id="product__name">실시간 배전반 관리 시스템</span>
-                    <span id="login__title">Login to your Account</span>
+        <div id="login__body">
+            <div id="login__container">
+                <div className="login__wrapper" id="wrapper__left">
+                    <div id="login__header">
+                        <span id="company__name">T/O Solution</span>
+                        <span id="product__name">실시간 배전반 관리 시스템</span>
+                        <span id="login__title">Login to your Account</span>
+                    </div>
+                    <div id="login__form">
+                        <form>
+                            <span>ID</span>
+                            <input type="text" placeholder="아이디를 입력하세요" />
+                            <span>Password</span>
+                            <input type="password" placeholder="비밀번호를 입력하세요" />
+                            <div id="remember__check"><input type="checkbox" /><span>Remember me</span></div>
+                            <button type="submit" onClick={() => navigate("/Main")}>Login</button>
+                        </form>
+                    </div>
                 </div>
-                <div id="login__form">
-                    <form>
-                        <span>ID</span>
-                        <input type="text" />
-                        <span>Password</span>
-                        <input type="password" />
-                        <div id="remember__check"><input type="checkbox" /><span>Remember me</span></div>
-                        <button type="submit" onClick={() => navigate("/Main")}>Login</button>
-                    </form>
+                <div className="login__wrapper" id="wrapper__right">
                 </div>
-            </div>
-            <div className="login__wrapper" id="wrapper__right">
             </div>
         </div>
     );
